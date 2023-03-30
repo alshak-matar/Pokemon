d763e9bd8e7ed857b864d8540b7fc4e37c1c92ff
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-30 18:39:45 +0200

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -118,18 +118,19 @@ class Pokemon:
 
 
 
-
 if __name__ == '__main__':
-    Dracaufeu = Pokemon('Dracaufeu', 'Feu', ['Lance-flammes', 'voler', ' Brûlure', 'Coup de feu'], {'ATTACK':12, 'DEFENSE': 8})
-    Blastoise = Pokemon('Blastoise', 'Eau', ['Pistolet à eau', 'Faisceau de bulle', 'Pompe hydraulique', 'Le surf'], {'ATTACK':10, 'DEFENSE': 10})
-    Florizarre = Pokemon('Florizarre', 'Herbe', ['Lingette de vigne', 'Feuille de rasoir', 'Tremblement de terre', 'Plante frénétique'], {'ATTACK':8, 'DEFENSE': 12})
+  
+    Charizard = Pokemon('Charizard', 'Fire', ['Flamethrower', 'Fly', 'Blast Burn', 'Fire Punch'], {'ATTACK':12, 'DEFENSE': 8})
+    Blastoise = Pokemon('Blastoise', 'Water', ['Water Gun', 'Bubblebeam', 'Hydro Pump', 'Surf'],{'ATTACK': 10, 'DEFENSE':10})
+    Venusaur = Pokemon('Venusaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Earthquake', 'Frenzy Plant'],{'ATTACK':8, 'DEFENSE':12})
 
-    Salamèche = Pokemon('Salamèche', 'Feu', ['braise', 'Gratter', 'Attaquer', 'Coup de feu'], {'ATTACK':4, 'DEFENSE': 2})
-    Carapuce = Pokemon('Carapuce', 'Eau', ['Faisceau de bulle', 'Attaquer', 'Coup de tête', 'Le surf',], {'ATTACK':3, 'DEFENSE': 3})
-    Bulbizarre = Pokemon('Bulbizarre', 'Herbe', ['Lingette de vigne', 'Feuille de rasoir', 'Attaquer', 'Graine de sangsue',], {'ATTACK':2, 'DEFENSE': 4})
+    Charmander = Pokemon('Charmander', 'Fire', ['Ember', 'Scratch', 'Tackle', 'Fire Punch'],{'ATTACK':4, 'DEFENSE':2})
+    Squirtle = Pokemon('Squirtle', 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf'],{'ATTACK': 3, 'DEFENSE':3})
+    Bulbasaur = Pokemon('Bulbasaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Tackle', 'Leech Seed'],{'ATTACK':2, 'DEFENSE':4})
 
-    Charméléon = Pokemon('Charméléon', 'feu', ['braise', 'Gratter', 'Lance-flammes', 'Coup de feu',], {'ATTACK':6, 'DEFENSE': 5})
-    Tortue = Pokemon('Tortue', 'Eau', ['Faisceau de bulle', 'Pistolet à eau', 'Pistolet à eau', 'Le Surf',], {'ATTACK':5, 'DEFENSE': 5})
-    Ivysaure = Pokemon('Ivysaure\t', 'Herbe', ['Lingette de vigne', 'Feuille de rasoir', 'Graine de balle', 'Graine de sangsue',], {'ATTACK':4, 'DEFENSE': 6})
+    Charmeleon = Pokemon('Charmeleon', 'Fire', ['Ember', 'Scratch', 'Flamethrower', 'Fire Punch'],{'ATTACK':6, 'DEFENSE':5})
+    Wartortle = Pokemon('Wartortle', 'Water', ['Bubblebeam', 'Water Gun', 'Headbutt', 'Surf'],{'ATTACK': 5, 'DEFENSE':5})
+    Ivysaur = Pokemon('Ivysaur\t', 'Grass', ['Vine Wip', 'Razor Leaf', 'Bullet Seed', 'Leech Seed'],{'ATTACK':4, 'DEFENSE':6})
 
-    Dracaufeu.fight(Blastoise)
+
+    Charizard.fight(Blastoise) 
