4d7f9c3505e802cb5d72596dc931c23dc679a89b
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-30 14:38:26 +0200

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -41,24 +41,24 @@ class Pokemon:
         for i,k in enumerate(version):
             if self.types == k:
                 if Pokemon2.types == k:
-                    string_1_attack = '\n Its not very effective...'
-                    string_2_attack = '\n Its not very effective...'
+                    string_1_attack = '\n Ce n est pas très efficace...'
+                    string_2_attack = '\n Ce n est pas très efficace...'
 
                 if Pokemon2.types == version[(i+1)%3]:
                     Pokemon2.attack *= 2
                     Pokemon2.defense *= 2
                     self.attack /= 2
                     self.defense /= 2
-                    string_1_attack = '\n Its not very effective'
-                    string_2_attack = '\n Its super effective!'
+                    string_1_attack = '\n Ce n est pas très efficace'
+                    string_2_attack = '\n C est super efficace!'
                 
                 if Pokemon2.types == version[(i+1)%3]:
                     self.attack *= 2
                     self.defense *= 2
                     Pokemon2.attack /= 2
                     Pokemon2.attack /= 2
-                    string_1_attack = '\n Its super effective'
-                    string_2_attack = '\n Its not very effective'
+                    string_1_attack = '\n C est super efficace'
+                    string_2_attack = '\n Ce n est pas très efficace...'
             while (self.bars) and (Pokemon2.bars > 0):
                 print(f"{self.name}\t\tHLTH\t{self.health}")
                 print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
@@ -66,7 +66,7 @@ class Pokemon:
                 print(f"Go {self.name}!")
                 for i, x in enumerate(self.moves):
                     print(f"{i+1}.", x)
-                index = int(input('Pick a move: '))
+                index = int(input('Choisissez un mouvement: '))
                 delay_print(f"\n{self.name} used {self.moves[index-1]}!")
                 time.sleep(1)
                 delay_print(string_1_attack)
@@ -80,9 +80,9 @@ class Pokemon:
 
                 time.sleep(1)
 
-                print(f"{self.name}\t\tHLTH\t{self.health}")
-                print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
-                time.sleep(0.5)
+                print(f"\n{self.name}\t\tHLTH\t{self.health}")
+                print(f"\n{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
+                time.sleep(.5)
 
                 if Pokemon2.bars <= 0:
                     delay_print("\n..." + Pokemon2.name + 'fainted.')
@@ -114,22 +114,22 @@ class Pokemon:
                     delay_print("\n..." + self.name + 'fainted.')
                     break
             money = np.random.choice(5000)
-            delay_print(f"\n Opponent paid you €{money}.")
+            delay_print(f"\n L'adversaire vous a payé €{money}.")
 
 
 
 
 if __name__ == '__main__':
-    Charizard = Pokemon('Charizard', 'Fire', ['Flamethrower', 'Fly', 'Blast Burn', 'Fire Punch'], {'ATTACK':12, 'DEFENSE': 8})
-    Blastoise = Pokemon('Blastoise', 'Water', ['Water Gun', 'Bubblebeam', 'Hydro Pump', 'Surf'], {'ATTACK':10, 'DEFENSE': 10})
-    Venusaur = Pokemon('Venusaur', 'Grass', ['Vine Wip', 'Razor leaf', 'Earthquak', 'Frenzy Plant'], {'ATTACK':8, 'DEFENSE': 12})
+    Dracaufeu = Pokemon('Dracaufeu', 'Feu', ['Lance-flammes', 'voler', ' Brûlure', 'Coup de feu'], {'ATTACK':12, 'DEFENSE': 8})
+    Blastoise = Pokemon('Blastoise', 'Eau', ['Pistolet à eau', 'Faisceau de bulle', 'Pompe hydraulique', 'Le surf'], {'ATTACK':10, 'DEFENSE': 10})
+    Florizarre = Pokemon('Florizarre', 'Herbe', ['Lingette de vigne', 'Feuille de rasoir', 'Tremblement de terre', 'Plante frénétique'], {'ATTACK':8, 'DEFENSE': 12})
 
-    Charmander = Pokemon('Charmander', 'Fire', ['Ember', 'Ember', 'Scratch', 'Tackle', 'Fire Punch'], {'ATTACK':4, 'DEFENSE': 2})
-    Squirtle = Pokemon('Squirtle', 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf',], {'ATTACK':3, 'DEFENSE': 3})
-    Bulbasaur = Pokemon('Bulbasaur', 'Grass', ['Vine Wip', 'Razor leaf', 'Tackle', 'leech Seed',], {'ATTACK':2, 'DEFENSE': 4})
+    Salamèche = Pokemon('Salamèche', 'Feu', ['braise', 'Gratter', 'Attaquer', 'Coup de feu'], {'ATTACK':4, 'DEFENSE': 2})
+    Carapuce = Pokemon('Carapuce', 'Eau', ['Faisceau de bulle', 'Attaquer', 'Coup de tête', 'Le surf',], {'ATTACK':3, 'DEFENSE': 3})
+    Bulbizarre = Pokemon('Bulbizarre', 'Herbe', ['Lingette de vigne', 'Feuille de rasoir', 'Attaquer', 'Graine de sangsue',], {'ATTACK':2, 'DEFENSE': 4})
 
-    Charmeleon = Pokemon('Charmeleon', 'Fire', ['Ember', 'Scratch', 'Flamethrower', 'Fire Punch',], {'ATTACK':6, 'DEFENSE': 5})
-    Wartortle = Pokemon('Wartortle', 'Water', ['Bubblebeam', 'Water Gun', 'Headbutt', 'Surf',], {'ATTACK':5, 'DEFENSE': 5})
-    Ivysaur = Pokemon('Ivysaur\t', 'Grass', ['Vine Wip', 'Razor leaf', 'Bullet Seed', 'leech Seed',], {'ATTACK':4, 'DEFENSE': 6})
+    Charméléon = Pokemon('Charméléon', 'feu', ['braise', 'Gratter', 'Lance-flammes', 'Coup de feu',], {'ATTACK':6, 'DEFENSE': 5})
+    Tortue = Pokemon('Tortue', 'Eau', ['Faisceau de bulle', 'Pistolet à eau', 'Pistolet à eau', 'Le Surf',], {'ATTACK':5, 'DEFENSE': 5})
+    Ivysaure = Pokemon('Ivysaure\t', 'Herbe', ['Lingette de vigne', 'Feuille de rasoir', 'Graine de balle', 'Graine de sangsue',], {'ATTACK':4, 'DEFENSE': 6})
 
-    Charizard.fight(Blastoise)
+    Dracaufeu.fight(Blastoise)
